08ccca91c1588e14c827df04e4e8b91cae7568f3
Iryna Omelchenko <iren.omelchenko@gmail.com>
2021-03-11 11:19:35 +0200

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        arr.push(bracketsConfig[i].join(''));
+    }
+    for (let y = str.length; y >= 0; y--) {
+        for (let j = 0; j < arr.length; j++) {
+            if (str.includes(arr[j])) {
+                str = str.replace(arr[j], '');
+            }
+        }
+    }
+    return str === '';
 }
